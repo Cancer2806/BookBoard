68a33a1c5635c78c825b3d8e08cd0fec26c64837
Frank Lavery <frank.lavery@westnet.com.au>
2022-05-23 21:22:14 +0800

    initial file structure connection_js server_js schema_sql package

---
+++ config/connection.js
@@ -0,0 +1,24 @@
+// Use sequelize as the ORM for managing the database
+const Sequelize = require('sequelize');
+// Use dotenv to substitute environment variables for security purposes
+require('dotenv').config();
+
+let sequelize;
+
+// Configure database connection
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    }
+  );
+}
+
+module.exports = sequelize;
