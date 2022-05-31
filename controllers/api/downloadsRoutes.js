8caff9e528da4a18b563a9a32adbf17daaae41d0
Vinitha Gowtheepan <sreevinithaa@gmail.com>
2022-06-01 05:50:58 +0800

    functionality update-checking existing record before add duplicate in downloads and favourites

--- controllers/api/downloadsRoutes.js
+++ controllers/api/downloadsRoutes.js
@@ -4,15 +4,19 @@ const { Downloads } = require('../../models');
 //Add new downloads method
 router.post('/', async (req, res) => {
     try {
-      
-      const downloadData = await Downloads.create({
+      const data=await Downloads.findOne({where:{file_id:req.body.file_id,user_id:1}});
+      if(!data)
+      { const downloadData = await Downloads.create({
         price:req.body.price,
         user_id: 1,
         file_id:req.body.file_id,
       
       });
-  
       res.status(200).json(downloadData);
+      }
+     
+  
+      res.status(200).json(data);
     } catch (err) {
       res.status(400).json(err);
     }
